c0d9606397513c83a668e6291c8147dcb91bae1d
Samyak Jain <samtan106@gmail.com>
2020-04-13 21:44:07 +0530

    Bug Fixes

--- src/index.ts
+++ src/index.ts
@@ -2,37 +2,51 @@ import * as AgoraRTM from 'agora-rtm-sdk';
 import AgoraOffice, { Role } from 'agora-office';
 
 const client = AgoraRTM.createInstance("029250729b864e369e870703ac3fd265");
-client.login({uid: prompt("name")}).then(() => {
-    console.log('AgoraRTM client login success');
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const textBox: HTMLInputElement = <HTMLInputElement> document.getElementById("uid");
 
-    const channel = client.createChannel("demo");
-    const iframe: HTMLIFrameElement = document.getElementsByTagName("iframe")[0];
-    const radios = document.querySelectorAll("input[type=radio]");
+startButton.addEventListener('click', () => {
+    startButton.classList.toggle('disabled');
 
-    radios.forEach(value => {
-        value.addEventListener('change', event => {
-            const element: HTMLInputElement = <HTMLInputElement> event.target;
-            if (element.value == "broadcaster") {
-                const officeClient = new AgoraOffice("https://dry-journey-00420.herokuapp.com", channel, iframe, Role.Broadcaster);
-                officeClient.renderFilePickerUi().then((fileUrl: URL) => {
-                    console.log("Rendered")
-                    officeClient.loadDocument(fileUrl).then(() => {
-                        console.log("Document loaded");
-                        officeClient.syncDocument();
-                    }).catch(error => {
-                        console.log("Error in loading document");
-                        console.log(error);
-                    });
-                }).catch(error => {
-                    console.log("Error in rendering " + error);
-                });
-            } else {
-                const officeClient = new AgoraOffice("https://dry-journey-00420.herokuapp.com", channel, iframe, Role.Receiver);
-                officeClient.syncDocument();
-            }
+    const radios: HTMLInputElement = document.querySelector("input[type=radio]:checked");
+
+    client.login({uid: textBox.value}).then(() => {
+        console.log('AgoraRTM client login success');
+    
+        const channel = client.createChannel("demo");
+        const iframe: HTMLIFrameElement = document.getElementsByTagName("iframe")[0];
+        const officeClient = new AgoraOffice("https://dry-journey-00420.herokuapp.com", channel, iframe);
+
+        stopButton.addEventListener("click", () => {
+            stopButton.classList.toggle('disabled');
+            officeClient.stopSync();
         });
+
+        console.log(radios.value);
+    
+        if (radios.value == "broadcaster") {
+            officeClient.setRole(Role.Broadcaster);
+            officeClient.renderFilePickerUi().then((fileUrl: URL) => {
+                console.log("Rendered")
+                officeClient.loadDocument(fileUrl).then(() => {
+                    console.log(fileUrl);
+                    console.log("Document loaded");
+                    officeClient.syncDocument();
+                }).catch(error => {
+                    console.log("Error in loading document");
+                    console.log(error);
+                });
+            }).catch(error => {
+                console.log("Error in rendering " + error);
+            });
+        } else {
+            officeClient.setRole(Role.Receiver);
+            officeClient.syncDocument();
+        }
+            
+    }).catch(err => {
+        console.log('AgoraRTM client login failure', err);
     });
-}).catch(err => {
-    console.log('AgoraRTM client login failure', err);
 });
 
